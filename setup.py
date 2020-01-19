607b1664f2fd0cf0732ee424b04e13dcd948553c
Afiz Oyerinde <45148604+aoyerinde@users.noreply.github.com>
2020-01-19 21:31:21 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Data_validation',
+    url='https://github.com/aoyerinde/Data_validation_v1',
+    author='Afiz Oyerinde',
+    author_email='ayodejioyerinde93@gmail.com',
+    # Needed to actually package something
+    packages=['data_validation'],
+    # Needed for dependencies
+    install_requires=['numpy', 'pandas'],
+    # *strongly* suggested for sharing
+    version='1.0',
+    # The license can be anything you like
+    license='None',
+    description='Data validation package',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
